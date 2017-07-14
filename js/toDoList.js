708797da951a0be45c9ed7483824b0e6faadf70c
Eoghan O'Neill <eoneill@palantirsolutions.com>
2017-07-14 17:31:24 +0100

    finished mvc changes

--- js/toDoList.js
+++ js/toDoList.js
@@ -4,18 +4,25 @@
   "use strict";  
   
   var view = (function() {
-
+    
     function addCatToCombo(cat) {
-      var el, $catOptions;
-      $catOptions = document.getElementById("usedCategories");
-      el = document.createElement("option");
-      el.textContent = cat;
-      $catOptions.appendChild(el);
+      var el, $catOptions, optionFound = false;
+      $catOptions = document.getElementById(self.$catSelect.list.id);
+      for (var j = 0; j < $catOptions.options.length; j++) {
+        if (cat == $catOptions.options[j].innerHTML) {
+          optionFound = true;
+          break;
+        }
+      }
+      if (!optionFound) {
+        el = document.createElement("option");
+        el.textContent = cat;
+        $catOptions.appendChild(el);
+      }      
     }
 
-    function refreshList() {
-      var toDoList = getSavedList();
-      $toDoList.innerHTML = "";
+    function refreshList(toDoList) {      
+      view.$toDoList.innerHTML = "";
       toDoList.forEach(function(element) {
         addItemToList(element);
       }, this);    
@@ -25,22 +32,24 @@
       var li = document.createElement("li");
       li.value = task.name + "_" + task.dateCreated;
       li.textContent = `Category: ${task.category}, Task: ${task.name}`;
-      if ($toDoList.hasChildNodes()) {
-        $toDoList.insertBefore(li, $toDoList.firstChild);
+      if (view.$toDoList.hasChildNodes()) {
+        view.$toDoList.insertBefore(li, view.$toDoList.firstChild);
       } else {
-        $toDoList.appendChild(li);
+        view.$toDoList.appendChild(li);
       }
     }
 
-    return {
+    var self = {
       addCatToCombo : addCatToCombo, refreshList : refreshList, addItemToList : addItemToList,
-      form : document.getElementById("newTaskForm"),
-      createDummyTasks : document.getElementById("createDummyTasks"),
-      clearTasks : document.getElementById("clearStorage"),
-      toDoList : document.getElementById("toDoList"),
-      newTask : document.getElementById("newTask"),
-      catSelect : document.getElementById("categoryChooser")
+      $form : document.getElementById("newTaskForm"),
+      $createDummyTasks : document.getElementById("createDummyTasks"),
+      $clearTasks : document.getElementById("clearStorage"),
+      $toDoList : document.getElementById("toDoList"),
+      $newTask : document.getElementById("newTask"),
+      $catSelect : document.getElementById("categoryChooser")
     };
+
+    return self;
   }());
     
 
@@ -48,25 +57,25 @@
   function ToDoList() {
     "use strict";    
 
-    this.allTasks = []; //initialise task array
+    this._allTasks = []; //initialise task array
     
   }
 
   ToDoList.prototype.getTaskList = function (forceRefresh) {
-    if (!this.allTasks.length || refreshFromStorage) {
+    if (!this._allTasks.length || forceRefresh) {
       var i = localStorage.length;
       while (i--) {
-        this.allTasks.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        this._allTasks.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
       }
-      this.allTasks.sort((x,y) => x.dateCreated >= y.dateCreated);
+      this._allTasks.sort((x,y) => x.dateCreated >= y.dateCreated);
     }
     
-    return this.allTasks;
+    return this._allTasks;
   }
 
-  ToDoList.prototype.getCategories = function (taskList) {
+  ToDoList.prototype.getCategories = function () {
     //get the unique list of used categories, plus add a few
-    let allCats = taskList.map(task => task.category);
+    let allCats = this._allTasks.map(task => task.category);
     allCats.forEach(x=>console.log(x));
     allCats = allCats.filter((cat, i, arr) => arr.indexOf(cat) === i);//remove duplicates
     if (allCats.filter(x => x.toLowerCase().trim() === "general") !== -1) {
@@ -78,8 +87,13 @@
 
   ToDoList.prototype.saveItem = function(task) {
     //use timestamp as key - update task if it already exists
-    allTasks.push(task);   
+    this._allTasks.push(task);   
     localStorage.setItem(task.name + "_" + task.dateCreated, JSON.stringify(task));
+  }
+
+  ToDoList.prototype.deleteAllTasks = function() {
+    this._allTasks.length = 0;
+    localStorage.clear();
   }
   
   //Constructors
@@ -92,32 +106,36 @@
   }
   
   //Controller
-  function Controller(model, view) {
+  function Controller(model) {
   
     var self = this;
     self.model = model;
     self.view = view;
     //populate categories in the view
-    self.model.getCategories(self.model.getTaskList()).forEach(function(cat) {
-      self.view.addCatToCombo(cat);
-    });
+    updateCategoryCombo();
 
-    view.form.addEventListener("submit", saveTaskHandler);
-    view.createDummyTasks.addEventListener("click", createDummyTasks);
-    view.clearTasks.addEventListener("click", clearTasksHandler);
-    view.toDoList.addEventListener("click", taskDoneHandler);
+    view.$form.addEventListener("submit", saveTaskHandler);
+    view.$createDummyTasks.addEventListener("click", createDummyTasks);
+    view.$clearTasks.addEventListener("click", clearTasksHandler);
+    view.$toDoList.addEventListener("click", taskDoneHandler);
     window.addEventListener("storage", storageHandler);
-    view.refreshList();
+    view.refreshList(model.getTaskList());
+
+    function updateCategoryCombo() {
+      self.model.getCategories().forEach(function(cat) {
+        self.view.addCatToCombo(cat);
+      });
+    }
 
     //event handlers
     function saveTaskHandler(evt) {
       evt.preventDefault();    
     
-      var newTask = new ToDoTask({name: self.view.newTask.value, category: self.view.catSelect.value});
+      var newTask = new ToDoTask({name: self.view.$newTask.value, category: self.view.$catSelect.value});
       self.view.addItemToList(newTask);    
-      self.model.saveItemToStorage(newTask);
-      self.view.addCatToCombo($catSelect.value);
-      self.view.newTask.value = "";
+      self.model.saveItem(newTask);
+      self.view.addCatToCombo(view.$catSelect.value);
+      self.view.$newTask.value = "";
     }
 
     function taskDoneHandler(evt) {
@@ -130,8 +148,8 @@
 
     function clearTasksHandler(evt) {
         evt.preventDefault();
-        localStorage.clear();
-        self.view.refreshList();
+        self.model.deleteAllTasks();
+        self.view.refreshList(model.getTaskList(true));
     }
 
     function createDummyTasks(evt) {
@@ -143,18 +161,19 @@
         category: "Dev"});
       var task3 = new ToDoTask({name: "Plant out sunflower seeds",
         category: "Garden"});
-      model.saveItemToStorage(task1);
-      model.saveItemToStorage(task2);
-      model.saveItemToStorage(task3);
-      
-      self.view.refreshList();
+      model.saveItem(task1);
+      model.saveItem(task2);
+      model.saveItem(task3);
+      self.updateCategoryCombo();
+
+      view.refreshList(model.getTaskList());
     }   
   } 
 
   //entry point
   (function initialise() {
     var model = new ToDoList();
-    var controller = new Controller(model, view);   
+    var controller = new Controller(model);   
     
   })();
   
