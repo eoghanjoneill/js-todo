9d7dbc765285db0f6d79cfb2514ed471ae6ac2dd
Eoghan O'Neill <eoghanjoneill@gmail.com>
2017-09-24 22:16:38 +0100

    allow CORS

--- tests/testPost.js
+++ tests/testPost.js
@@ -3,16 +3,17 @@ var request = require("request");
 var HEROES = {
     _userName: "HeroArrayTest",
     heroes: [
-    { id: 11, name: 'Hercules' },
-    { id: 12, name: 'Aeneas' },
-    { id: 13, name: 'Romulus' },
-    { id: 14, name: 'Remus' },
-    { id: 15, name: 'Publius Scipio Africanus the Elder' },
-    { id: 16, name: 'Nero' },
-    { id: 17, name: 'Venus' },
-    { id: 18, name: 'Mars' },
-    { id: 19, name: 'Julius Caesar' },
-    { id: 20, name: 'Fionn MacChumhaill' }
+    { id: 0, name: 'Zero'},
+      { id: 11, name: 'Hercules'},
+      { id: 12, name: 'Aeneas'},
+      { id: 13, name: 'Romulus'},
+      { id: 14, name: 'Remus'},
+      { id: 15, name: 'Publius Scipio Africanus the Elder'},
+      { id: 16, name: 'Nero'},
+      { id: 17, name: 'Venus'},
+      { id: 18, name: 'Mars'},
+      { id: 19, name: 'Julius Caesar'},
+      { id: 20, name: 'Fionn MacChumhaill'}
 ]};
 
 request.post(
