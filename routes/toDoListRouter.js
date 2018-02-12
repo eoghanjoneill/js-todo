3c687077ecd5ce5b2c22eab05c27d3a04a607b96
Eoghan O'Neill <eoghanjoneill@gmail.com>
2018-02-12 20:36:36 +0000

    mongoose stuff - refactor to promises on the toDoListRepository

--- routes/toDoListRouter.js
+++ routes/toDoListRouter.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var toDoListService = require("../lib/dbAccess.js");
-var resGen = require("../lib/responseGenerator.js");
+var listRepo = require('../lib/toDoListRepository');
+var resGen = require('../lib/responseGenerator.js');
 
 router.get(/^\/?$/i, getToDoListHandler);
 router.get(/^\/(\w+)\/?$/i, getToDoListByNameHandler);
@@ -10,34 +11,33 @@ module.exports = router;
 
 //Route handlers
 function getToDoListHandler(req, res, next) {
-  toDoListService.getToDoLists (function (error, data) {
-      if (error) {      
-        resGen.send500("Error getting the lists", res);
-      }
-      else {
-        resGen.sendJson(data, res);
-      }
-    });
+  listRepo.getToDoLists().then(data => {
+    resGen.sendJson(data, res);
+  })
+  .catch(error => {
+    resGen.send500("Error getting the lists", res);
+  });  
 }
 
 function getToDoListByNameHandler(req, res, next) {
-  var listId = req.params[0];  
-  toDoListService.getToDoListByName(listId, function(error, data) {
-    if (error) {
+  var listId = req.params[0];
+  listRepo.getToDoList(listId)
+    .then(data => {
+      if (!data) {
+        resGen.send404("List " + listId, res);
+      }
+      else {
+        resGen.sendJson(data, res);
+      }      
+    })
+    .catch(error => {
       resGen.send500("Error getting list " + listId, res);
-    }
-    else if (!data) {
-      resGen.send404("List " + listId, res);
-    }
-    else {
-      resGen.sendJson(data, res);
-    }      
-  });
+    });
 }
 
 function postUpsertToDoListHandler(req, res, next) {
   console.log("Tried to add one!");
-  toDoListService.upsertToDoList(req.body, function(error, data) {
+  listRepo.upsertToDoList(req.body, function(error, data) {
     if (error) {
       resGen.send500("Error saving list to database.", res);
     }    
